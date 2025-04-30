8935022590211ac9aa65e20f2ed1c69746c84ded
scoz <coz.steven@gmail.com>
2025-04-30 12:55:41 +0200

    Modification cv.json et pipeline

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,8 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://stvncz.github.io',
+  base: 'portfolio',
+  trailingSlash: 'always',
   integrations: [tailwind()],
 });
