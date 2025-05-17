cbfc98aa0d5c7aab170d1b49761420bc93e8fc2d
scoz <coz.steven@gmail.com>
2025-05-17 10:38:48 +0200

    Update base path in astro.config.mjs to root for correct deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://stvncz.github.io',
-  base: 'portfolio',
+  base: '/',
   trailingSlash: 'always',
   integrations: [tailwind()],
 });
